585eeb8c70818296483947d85b648bdd2fee8cad
md.munna <mk8761174@gmail.com>
2025-09-15 15:14:49 +0600

    49-5 One-to-Many Relation in Prisma

---
+++ prisma/migrations/20250915091005_one_to_many_relation/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `author` on the `Post` table. All the data in the column will be lost.
+  - Added the required column `authorId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Post" DROP COLUMN "author",
+ADD COLUMN     "authorId" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
